251c233088c634a6c91949a1bfcc4c1bc29a5a1d
Mayra Salazar <mayrasalazar@unitec.edu>
2016-11-25 13:26:05 -0600

    Comenzando lab6

---
+++ Person.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+using std::string;
+class Person
+{
+private:
+	string name;
+	string surname;
+	string nickname;
+	int level;
+public:
+	Person();
+	void setName(string);
+	string getName();
+	void setSurname(string);
+	string getSurname();
+	void setNickname(string);
+	string getNickname();
+	void setLevel(int);
+	int getLevel();
+	~Person();
+	
+};
